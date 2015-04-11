cab3a3cfe6ce1874ad94fb1cac5718fa1016f8ce
Russ <c1728p9@gmail.com>
2015-04-11 00:33:19 -0500

    Compute CRCs to analyze flash faster
    When deciding between chip erase programming and sector erase
    programming an estimate for the number of pages with the same
    data must be known.  Previously this was done by reading
    the first 32 bytes of each page and comparing that to the new
    data.  This gives a good approximation, but does not catch all
    changes.
    
    This patch adds a binary which computes the CRC of each sector that
    is going to be programmed.  By calling flash.computeCrcs() a CRC
    for each page is computed.  This both decreases analysis time and
    greatly increases estimate accuracy.  This also prevents
    unnecessarily checking pages that have differing data, since
    this can be reliably determined from the CRC.

--- pyOCD/flash/flash.py
+++ pyOCD/flash/flash.py
@@ -25,12 +25,36 @@ DEFAULT_PAGE_PROGRAM_WEIGHT = 0.130
 DEFAULT_PAGE_ERASE_WEIGHT   = 0.048
 DEFAULT_CHIP_ERASE_WEIGHT   = 0.174
 
+# Program to compute the CRC of sectors.  This works on cortex-m processors.
+# Code is relocatable and only needs to be on a 4 byte boundary.
+# 212 bytes of executable data below + 1024 byte crc table = 1236 bytes
+# Usage requirements:
+# -In memory reserve 0x600 for code & table
+# -Make sure data buffer is big enough to hold 4 bytes for each page that could be checked (ie.  >= num pages * 4)
+analyzer = (
+    0x1c04b5f8, 0x25001c0e, 0xf82af000, 0x2e002701, 0x8822d00d, 0x40931c3b, 0x88631c1a, 0x1c192000,
+    0xf0004351, 0x3501f807, 0x42b5c401, 0xbe00d1f1, 0xbdf82000, 0x43c0b530, 0xd00d2a00, 0x188a4d07,
+    0x24ff447d, 0x3101780b, 0x40234043, 0x595b009b, 0x40580a00, 0xd1f54291, 0xbd3043c0, 0x00000090,
+    0x4e1ab5f0, 0x4b1a2780, 0x2100447e, 0x007f2201, 0x40081c10, 0x084c4240, 0x40604018, 0x40100844,
+    0x401d4245, 0x08684065, 0x426c4015, 0x4044401c, 0x40140865, 0x40184260, 0x08444068, 0x42454010,
+    0x4065401d, 0x40150868, 0x401c426c, 0x08654044, 0x42604014, 0x40684018, 0x40100844, 0x40184240,
+    0x31014060, 0x42b9c601, 0xbdf0d1d2, 0x00000068, 0xedb88320,
+    )
+
+def _msb( n ):
+    ndx = 0
+    while ( 1 < n ):
+        n = ( n >> 1 )
+        ndx += 1
+    return ndx
+
 class PageInfo(object):
 
     def __init__(self):
         self.erase_weight = None        # Time it takes to erase a page
         self.program_weight = None      # Time it takes to program a page (Not including data transfer time)
         self.size = None                # Size of page
+        self.crc_supported = None       # Is the function computeCrcs supported?
 
 class FlashInfo(object):
 
@@ -61,6 +85,8 @@ class Flash(object):
 
         # download flash algo in RAM
         self.target.writeBlockMemoryAligned32(self.flash_algo['load_address'], self.flash_algo['instructions'])
+        if self.flash_algo['analyzer_supported']:
+            self.target.writeBlockMemoryAligned32(self.flash_algo['analyzer_address'], analyzer)
 
         # update core register to execute the init subroutine
         self.updateCoreRegister(0, 0, 0, 0, self.flash_algo['pc_init'])
@@ -75,6 +101,36 @@ class Flash(object):
             logging.error('init error: %i', result)
 
         return
+
+    def computeCrcs(self, sectors):
+
+        data = []
+
+        # Convert address, size pairs into commands
+        # for the crc computation algorithm to preform
+        for addr, size in sectors:
+            size_val = _msb(size)
+            addr_val = addr // size
+            # Size must be a power of 2
+            assert (1 << size_val) == size
+            # Address must be a multiple of size
+            assert (addr % size) == 0
+            val = (size_val << 0) | (addr_val << 16)
+            data.append(val)
+
+        self.target.writeBlockMemoryAligned32(self.begin_data, data)
+
+        # update core register to execute the subroutine
+        self.updateCoreRegister(self.begin_data, len(data), 0, 0, self.flash_algo['analyzer_address'])
+
+        # resume and wait until the breakpoint is hit
+        self.target.resume()
+        while(self.target.getState() == TARGET_RUNNING):
+            pass
+
+        # Read back the CRCs for each section
+        data = self.target.readBlockMemoryAligned32(self.begin_data, len(data))
+        return data
 
     def eraseAll(self):
         """
@@ -163,6 +219,7 @@ class Flash(object):
         info = FlashInfo()
         info.rom_start = 0
         info.erase_weight = DEFAULT_CHIP_ERASE_WEIGHT
+        info.crc_supported = self.flash_algo['analyzer_supported']
         return info
 
     def getFlashBuilder(self):
